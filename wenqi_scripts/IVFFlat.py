4417583dcd9ea721759e2a64846f0c30ad017b72
Ubuntu <ubuntu@ip-172-31-39-79.us-east-2.compute.internal>
2022-02-04 08:43:59 +0000

    add IVFFlat & IVFPQ

--- wenqi_scripts/IVFFlat.py
+++ wenqi_scripts/IVFFlat.py
@@ -1,6 +1,7 @@
 import faiss
 import time
 import sys
+import re
 import numpy as np
 
 from multiprocessing.dummy import Pool as ThreadPool
@@ -15,9 +16,13 @@ print("Loaded vector shape: ", vec.shape)
 # 1K as query vectors
 # 10K as training vectors (heuristic: train vec = max(train_vec, 100 * centroid_num))
 
-xb = vec[:500 * int(1e3)]
-xq = vec[500 * int(1e3): 501 * int(1e3)]
-xt = vec[501 * int(1e3): 511 * int(1e3)]
+nb = 500 * int(1e3)
+nq = 1 * int(1e3)
+nt = 50 * int(1e3)
+
+xb = vec[:nb]
+xq = vec[nb: nb + nq]
+xt = vec[nb + nq: nb + nq + nt]
 
 print("Shape: DB {} \t Query {} \t Training {}".format(
     xb.shape, xq.shape, xt.shape))
@@ -43,7 +48,8 @@ def matrix_slice_iterator(x, bs):
         lambda i01: x[i01[0]:i01[1]].astype('float32').copy(),
         block_ranges)
 
-index = faiss.IndexFlat(embedding_dim)
+print("Building Flat index for ground truth computation...")
+index_flat = faiss.IndexFlat(embedding_dim)
 
 i0 = 0
 t0 = time.time()
@@ -51,14 +57,94 @@ for xs in matrix_slice_iterator(xb, 100000):
     i1 = i0 + xs.shape[0]
     print('\radd %d:%d, %.3f s' % (i0, i1, time.time() - t0), end=' ')
     sys.stdout.flush()
-    index.add(xs)
+    index_flat.add(xs)
     i0 = i1
 
-print("Searching...")
-D_gt, I_gt = index.search(xq, 100)
-print("Distance (ground truth): ", D_gt[:10])
-print("Indices (ground truth): ", I_gt[:10])
+print("Searching for ground truth...")
+D_gt, I_gt = index_flat.search(xq, 100)
 
+n_dup = (D_gt[:, 0:1] == D_gt[:, 1:2]).sum()
+print("\nOut of {} queries, {} have duplicated nearest neighbor (tied distance)".format(
+    nq, n_dup))
+
+def choose_train_size(index_key):
+
+    # some training vectors for PQ and the PCA
+    n_train = 256 * 1000
+
+    if "IVF" in index_key:
+        matches = re.findall('IVF([0-9]+)', index_key)
+        ncentroids = int(matches[0])
+        n_train = max(n_train, 100 * ncentroids)
+    elif "IMI" in index_key:
+        matches = re.findall('IMI2x([0-9]+)', index_key)
+        nbit = int(matches[0])
+        n_train = max(n_train, 256 * (1 << nbit))
+    return n_train
+
+
+
+print("Building IVFFlat for ANNS...")
+nlist = 1024
+index_key = "IVF{},Flat".format(nlist)
+index_ivfflat = faiss.index_factory(embedding_dim, index_key)
+#quantizer = faiss.IndexFlat(embedding_dim)
+#index_ivfflat = faiss.IndexIVFFlat(quantizer, embedding_dim, nlist)
+
+xtsub = xt[:choose_train_size(index_key)]
+index_ivfflat.train(xtsub)
+i0 = 0
+t0 = time.time()
+for xs in matrix_slice_iterator(xb, 100000):
+    i1 = i0 + xs.shape[0]
+    print('\radd %d:%d, %.3f s' % (i0, i1, time.time() - t0), end=' ')
+    sys.stdout.flush()
+    index_ivfflat.add(xs)
+    i0 = i1
+
+for i in range(10):
+    nprobe = int(2 ** i)
+    print("\nnprobe = ", nprobe)
+    index_ivfflat.nprobe = nprobe
+    topK = 100
+    D, I = index_ivfflat.search(xq, topK)
+
+    for rank in 1, 10, 100:
+        if rank <= topK:
+            n_ok = (I[:, :rank] == I_gt[:, :1]).sum()
+            print("R@{} = {:.4f}".format(rank, (n_ok / float(nq))))
+# print("Distance (ground truth): ", D_gt[:10])
+#print("Indices (ground truth): ", I_gt[:10])
+
+
+
+print("Building IVF-PQ (16 byte quantization) for ANNS...")
+nlist = 1024
+index_key = "IVF{},PQ16".format(nlist)
+index_ivfpq = faiss.index_factory(embedding_dim, index_key)
+
+xtsub = xt[:choose_train_size(index_key)]
+index_ivfpq.train(xtsub)
+i0 = 0
+t0 = time.time()
+for xs in matrix_slice_iterator(xb, 100000):
+    i1 = i0 + xs.shape[0]
+    print('\radd %d:%d, %.3f s' % (i0, i1, time.time() - t0), end=' ')
+    sys.stdout.flush()
+    index_ivfpq.add(xs)
+    i0 = i1
+
+for i in range(10):
+    nprobe = int(2 ** i)
+    print("\nnprobe = ", nprobe)
+    index_ivfpq.nprobe = nprobe
+    topK = 100
+    D, I = index_ivfpq.search(xq, topK)
+
+    for rank in 1, 10, 100:
+        if rank <= topK:
+            n_ok = (I[:, :rank] == I_gt[:, :1]).sum()
+            print("R@{} = {:.4f}".format(rank, (n_ok / float(nq))))
 
 
 
